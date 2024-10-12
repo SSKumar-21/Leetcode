4ffc56edc3a95a0ea87043d50b38a66782e275fb
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-10-12 06:23:56 +0000

    Sync LeetCode submission Runtime - 7 ms (11.08%), Memory - 10.6 MB (73.12%)

---
+++ 0349-intersection-of-two-arrays/solution.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    int *res = malloc((nums1Size + nums2Size) * sizeof(int)); 
+    int check[2001]={0};
+    *returnSize=0;
+    int i,x=0;
+    for(i=0;i<nums1Size;i++){
+        check[nums1[i]+1000]++; 
+    }
+    for(i=0;i<nums2Size;i++){
+        if(check[nums2[i]+1000]){
+            res[x++]=nums2[i];
+            check[nums2[i]+1000]=0;
+            (*returnSize)++;
+        }
+    }
+    return res;
+}
