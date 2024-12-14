ea2b016aa727c63695e7c1b48d5dda64d9fa95f7
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-14 05:41:42 +0000

    Sync LeetCode submission Runtime - 13 ms (29.78%), Memory - 55.9 MB (99.96%)

---
+++ 0041-first-missing-positive/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        HashSet<Integer> set =new HashSet<>();
+        for(int i: nums){
+            if(i>=0){
+                set.add(i);
+            }
+        }
+        int i=1;
+        while(set.contains(i)){
+            i++;
+        }
+        return i;
+    }
+}
