08540d30503803e01cf7d8a2b591a46a500004d5
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-28 12:33:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (100.00%)

---
+++ 2298-count-integers-with-even-digit-sum/solution.c
@@ -0,0 +1,20 @@
+bool count(int n){
+    int sum=0;
+    while(n>0){
+        sum=sum+(n%10);
+        n/=10;
+    }
+    return sum%2==0;
+}
+int countEven(int num) {
+    if(num<2){
+        return 0;
+    }
+    int c=0;
+    for(int i=2;i<=num;i++){
+        if(count(i)){
+            c++;
+        }
+    }
+    return c;
+}
