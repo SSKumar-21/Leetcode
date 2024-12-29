8d9bd982a6a6a4b1ebec97f5a4380a245a768edc
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-29 06:24:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.1 MB (99.89%)

--- 2491-smallest-even-multiple/solution.java
+++ 2491-smallest-even-multiple/solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        for(int i=n;;i+=n){
-            if(i%2==0){
-                return i;
-            }
+        if(n%2==0){
+            return n;
         }
+        return 2*n;
     }
 }
