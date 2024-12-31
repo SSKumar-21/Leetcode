304542ebc1a490799c70b8650f7b0e07ce225050
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-31 12:36:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (100.00%)

---
+++ 0745-find-smallest-letter-greater-than-target/solution.c
@@ -0,0 +1,13 @@
+char nextGreatestLetter(char* letters, int lettersSize, char target) {
+    int l=0,r=lettersSize-1,mid;
+    while(l<=r){
+        mid=l+(r-l)/2;
+        if(letters[mid]>target){
+            r=mid-1;
+        } 
+        else{
+            l=mid+1;
+        }
+    }
+    return letters[l%lettersSize];
+}
