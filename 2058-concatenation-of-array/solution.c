1d7c822f5b12299e7539c59ae2815b68cafdba6b
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-01 07:13:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.6 MB (100.00%)

---
+++ 2058-concatenation-of-array/solution.c
@@ -0,0 +1,13 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+     int *arr=malloc(sizeof(int)*2*numsSize);
+     *returnSize=2*numsSize;;
+        for(int i=0;i<numsSize;i++){
+            *(arr+i)=nums[i];
+            *(arr+i+numsSize)=nums[i];
+        }
+        return arr;
+    }
+
