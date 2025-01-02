c901e7bd5576b7786f85e253ec95a083b05f9911
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-02 10:51:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.6 MB (100.00%)

---
+++ 2048-build-array-from-permutation/solution.c
@@ -0,0 +1,11 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize) {
+    int *arr=malloc(sizeof(int)*numsSize);
+    *returnSize=numsSize;
+    for(int i=0;i<numsSize;i++){
+        arr[i]=nums[nums[i]];
+    }
+    return arr;
+}
