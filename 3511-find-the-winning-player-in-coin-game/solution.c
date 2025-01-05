ef0698564cfcd7a93561a5133067d88abee72ed9
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-05 08:09:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

---
+++ 3511-find-the-winning-player-in-coin-game/solution.c
@@ -0,0 +1,12 @@
+char* winningPlayer(int x, int y) {
+    int c=0;
+    while(x>0 && y>3){
+        c++;
+        x--;
+        y-=4;
+    }
+    if(c%2!=0){
+        return "Alice";
+    }
+    return "Bob";
+}
