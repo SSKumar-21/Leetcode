88828046252584f22cf74b24ec258f81b6bceba3
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-06 07:03:10 +0000

    Sync LeetCode submission Runtime - 2 ms (81.12%), Memory - 45.8 MB (100.00%)

---
+++ 0944-smallest-range-i/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        int max=nums[0];
+    int min=nums[0];
+    
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]>max){
+           max=nums[i];
+        } 
+        if(nums[i]<min){
+            min=nums[i];
+        }
+    }
+    
+    return (min+k>=max-k)? 0 : max-k-min-k;
+    }
+}
