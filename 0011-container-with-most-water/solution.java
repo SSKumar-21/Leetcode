401fdf102a5da49c1e406550eceee9d158fad972
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-08 07:17:22 +0000

    Sync LeetCode submission Runtime - 4 ms (94.42%), Memory - 56.7 MB (99.92%)

---
+++ 0011-container-with-most-water/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+      int max=0;
+      int l=0,r=height.length -1;
+      while(l<r){
+        int h= (height[l]>height[r])? height[r] : height[l];
+        int area=(r-l)*h;
+        if(max<area){
+            max=area;
+        }
+        if(height[l]>height[r]){
+            r--;
+        } else{
+            l++;
+        }
+      }  
+      return max;
+    }
+}
