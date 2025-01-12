982330dd9589ef703600ca1a494b1b9ba9025b64
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-12 06:51:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.7 MB (99.95%)

---
+++ 2427-first-letter-to-appear-twice/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int[] arr = new int[26];
+        Arrays.fill(arr,0);
+        int i=0;
+        for(i=0; i<s.length();i++){
+            int key = (int)(s.charAt(i) - 'a');
+            arr[key]++;
+            if(arr[key]>1){
+                break;
+            }
+        }
+        return s.charAt(i);
+    }
+}
