535b08692d7ba52d172cd12837566335f325ee6b
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-28 06:53:26 +0000

    Sync LeetCode submission Runtime - 1 ms (36.06%), Memory - 41.7 MB (99.95%)

---
+++ 0551-student-attendance-record-i/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int a=0,l=0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='L'){
+                l++;
+                if(l>=3){
+                    return false;
+                }
+            } else if(c=='A'){
+                a++;
+                l=0;
+            } else {
+                l=0;
+            }
+        }
+        if(a>=2){
+            return false;
+        }
+        return true;
+    }
+}
