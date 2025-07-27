7680decc6331acb0fcc94e74d284cd02003a0566
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-07-27 04:55:31 +0000

    Sync LeetCode submission Runtime - 1 ms (99.36%), Memory - 42.3 MB (99.84%)

---
+++ 3396-valid-word/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch)) {
+                return false; 
+            }
+
+            if (Character.isLetter(ch)) {
+                char lowerChar = Character.toLowerCase(ch);
+                if ("aeiou".indexOf(lowerChar) >= 0) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
