1654ccafbbf687079ab24cf1e8d7ee5c95fc7d4e
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-12-29 13:35:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.4 MB (41.21%)

---
+++ 0061-rotate-list/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if(k==0 || head == null || head.next == null){
+            return head;
+        }
+
+        ListNode temp = head;
+        int len = 1;
+        while(temp.next != null){
+            temp = temp.next;
+            len++;
+        }
+        temp.next = head;
+        k = k%len;
+        temp = head;
+        for(int i=1;i<len - k;i++){
+            temp = temp.next;
+        }
+
+        head = temp.next;
+        temp.next = null;
+
+        return head;
+    }
+}
