ae375e34766a5d6f3581cb2b622addbef622fe48
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-03-02 03:12:23 +0000

    Sync LeetCode submission Runtime - 2 ms (99.37%), Memory - 49.7 MB (27.67%)

---
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minSwaps(int[][] grid) {
+        int n = grid.length;
+        int[] zeros = new int[n];
+        for(int i=0;i<n;i++){
+            int c = 0;
+            for(int j=n - 1;j>=0 && grid[i][j] == 0;j--){
+                c++;
+            }
+            zeros[i] = c;
+        }
+
+        int res = 0;
+
+
+        for(int i=0;i<n;i++){
+            int want = n -1 -i;
+            int j=i;
+            while(j<n && zeros[j] < want) j++;
+
+            if(j==n) return -1;
+
+            while(j>i){
+                int temp = zeros[j];
+                zeros[j] = zeros[j-1];
+                zeros[j-1] = temp;
+                j--;
+                res ++;
+            }
+        }
+        return res;
+    }
+}
