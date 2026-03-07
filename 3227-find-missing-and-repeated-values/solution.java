016dbfc09f83da25328791622758b1b9a0bb2991
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-03-07 14:15:11 +0000

    Sync LeetCode submission Runtime - 5 ms (36.78%), Memory - 47.4 MB (58.69%)

---
+++ 3227-find-missing-and-repeated-values/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        
+        int n = grid.length;
+        Set<Integer> set = new HashSet<>();
+        int[] res = new int[2];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(set.contains(grid[i][j])){
+                    res[0] = grid[i][j];
+                } else {
+                    set.add(grid[i][j]);
+                }
+            }
+        }
+
+        for(int i = 1; i <= n*n; i++){
+            if(!set.contains(i)){
+                res[1] = i;
+                break;
+            }
+        }
+
+        return res;
+    }
+}
