03cd1dd38d7120bfba43c1d1c4dd8e24a8a38f3f
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-03-24 13:23:51 +0000

    Sync LeetCode submission Runtime - 10 ms (96.38%), Memory - 100.9 MB (89.14%)

---
+++ 3031-construct-product-matrix/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] constructProductMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] p = new int[n][m];
+
+        long suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                p[i][j] = (int) suffix;
+                suffix = (suffix * grid[i][j]) % 12345;
+            }
+        }
+
+        long prefix = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                p[i][j] = (int) (((long) p[i][j] * prefix) % 12345);
+                prefix = (prefix * grid[i][j]) % 12345;
+            }
+        }
+
+        return p;
+    }
+}
