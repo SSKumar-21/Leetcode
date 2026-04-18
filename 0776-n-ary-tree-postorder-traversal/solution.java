247d706eda7bf83e8cc359c9b88ee4dab0810608
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-04-18 06:10:04 +0000

    Sync LeetCode submission Runtime - 3 ms (17.74%), Memory - 46.5 MB (29.89%)

---
+++ 0776-n-ary-tree-postorder-traversal/solution.java
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+
+        while(!st.isEmpty()){
+            Node node = st.pop();
+            res.add(node.val);
+            for(int i=0 ; i<=node.children.size() -1 ;i++){
+                Node kid = node.children.get(i);
+                st.push(kid);
+            }
+        }
+        return res.reversed();
+    }
+}
