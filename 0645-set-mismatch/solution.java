f15fdfbdab51ef33054fdf45932f2ceae3e77042
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-05-06 08:47:52 +0000

    Sync LeetCode submission Runtime - 13 ms (35.29%), Memory - 47.4 MB (88.01%)

---
+++ 0645-set-mismatch/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        int[] res = new int[2];
+        for(int x : nums){
+            if(set.contains(x)) res[0] = x;
+            set.add(x);
+        }
+
+        for(int i=1;i<=nums.length;i++){
+            if(!set.contains(i)) {
+                res[1] = i;
+                break;
+            }
+        }
+        return res;
+    }
+}
