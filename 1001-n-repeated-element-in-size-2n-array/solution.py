037686ef4d796a7e8c0a49243ca8f2478a643081
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-05-29 15:05:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.4 MB (29.57%)

--- 1001-n-repeated-element-in-size-2n-array/solution.py
+++ 1001-n-repeated-element-in-size-2n-array/solution.py
@@ -1,11 +1,9 @@
-class Solution(object):
-    def repeatedNTimes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        for i in nums:
-            if len(nums)/2 == nums.count(i):
-                return i
-        return -1
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        n = len(nums) -1
+        if (nums[0] == nums[n]): return nums[0]
+        for i in range(n) :
+            if(nums[i] == nums[i + 1]) or (nums[i] == nums[i + 2]): 
+                return nums[i]
         
+        return nums[0]
