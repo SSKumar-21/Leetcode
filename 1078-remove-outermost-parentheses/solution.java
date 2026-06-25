c1955eb70669ca880b17811f2f9f97e4410cb8c7
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-06-25 03:40:50 +0000

    Sync LeetCode submission Runtime - 10 ms (12.54%), Memory - 46.7 MB (13.61%)

---
+++ 1078-remove-outermost-parentheses/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count = 0;
+        String res = "" ;
+
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            
+            if (c == '('){
+                count++;
+                if(count > 1) res = res + '(' ;
+            } else {
+                count --;
+                if(count > 0) res = res + ')';
+            }
+
+        }
+        return res;
+    }
+}
