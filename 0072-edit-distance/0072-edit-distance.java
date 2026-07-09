093f2e4bcf5bf79bac40b5b85802ccadb8184da1
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-07-09 10:31:45 +0530

    Time: 6 ms (8.91%), Space: 47.2 MB (34.39%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,15 +2,6 @@ class Solution {
     public int minDistance(String a, String b) {
         int n = a.length();
         int m = b.length();
-
-        int[][] dp = new int[n + 1][m + 1];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-
-        return solve(a, b, n, m, dp);
-    }
-
-    private int solve(String a, String b, int n, int m, int[][] dp) {
         if (n == 0 && m == 0) {
             return 0;
         }
@@ -20,22 +11,28 @@ class Solution {
         if (m == 0) {
             return n;
         }
-        int res = 0;
-        if (dp[n - 1][m - 1] != -1) {
-            return dp[n - 1][m - 1];
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        for(int i=0;i<=n;i++) dp[i][0] = i;
+
+        for(int i=0;i<=m;i++) dp[0][i] = i;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(a.charAt(i-1) == b.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    int del_in = Math.min(dp[i][j-1], dp[i-1][j]);
+                    dp[i][j] = Math.min(dp[i-1][j-1], del_in) + 1; 
+                }
+            }
         }
-        if (a.charAt(n - 1) == b.charAt(m - 1)) {
 
-            res = solve(a, b, n - 1, m - 1, dp);
-
-        } else {
-
-            int i = solve(a, b, n, m - 1, dp);
-            int d = solve(a, b, n - 1, m, dp);
-            int r = solve(a, b, n - 1, m - 1, dp);
-            res = Math.min(i, d);
-            res = Math.min(res, r) + 1;
-        }
-        return dp[n - 1][m - 1] = res;
+        return dp[n][m];
     }
+
+    
 }
