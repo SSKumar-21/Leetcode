5aa3ca9812164f587b166586be2084435c5390f1
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-07-10 17:25:50 +0530

    Time: 6 ms (72.87%), Space: 45.3 MB (36.78%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,31 +1,30 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-         int sum = 0;
-        for(int ele:nums) sum+=ele;
-        if(sum < Math.abs(target) || (sum + target)%2!=0){
+        if (target < 0) target *= -1;
+        int sum = 0;
+        for (int ele : nums)
+            sum += ele;
+        if (sum < Math.abs(target) || (sum + target) % 2 != 0) {
             return 0;
         }
-        int t = (sum + target) / 2;
-        int[][] dp = new int[nums.length][t + 1];
-        for (int i = 0; i < nums.length; i++)  Arrays.fill(dp[i], -1);
+        int tar = (sum + target) / 2;
+        int n = nums.length;
+
+        int[][] dp = new int[n + 1][tar + 1];
+        dp[0][0] = 1;
+
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
         
-        return solve(0 , t ,nums,dp);
-    }
-
-    private int solve(int i, int t, int[] nums,int[][] dp){
-        if(i >= nums.length){
-            if (t == 0) return 1;
-            else return 0;
-        } 
-        if (dp[i][t] != -1) {
-            return dp[i][t];
+        for (int i = 1; i <= n; i++) {
+            for (int t = 0; t <= tar; t++) {
+                if (nums[i - 1] <= t) {
+                    dp[i][t] = dp[i - 1][t - nums[i - 1]] + dp[i - 1][t];
+                } else {
+                    dp[i][t] = dp[i - 1][t];
+                }
+            }
         }
 
-        int l = 0,r = 0;
-        if (nums[i] <= t){
-            l = solve(i + 1, t - nums[i], nums,dp);
-        }
-        r = solve(i+1 ,t ,nums,dp);
-        return dp[i][t] = l + r;
+        return dp[n][tar];
     }
 }
