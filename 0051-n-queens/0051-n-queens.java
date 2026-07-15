f84386b152b83820c7fc56b350d9e9f36ee2ed81
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2026-07-15 09:50:27 +0530

    Time: 6 ms (14.75%), Space: 47 MB (5.78%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        boolean[][] board = new boolean[n][n];
+        List<List<String>> res = new ArrayList<>();
+        solve(0, n, board, res);
+        return res;
+    }
+
+    private void solve(int row, int n, boolean[][] board, List<List<String>> res){
+        if(row == n){
+            List<String> temp = new ArrayList<>();
+            for(int i = 0;i < n; i++){
+                String str = "";
+                for(int j = 0;j<n;j++){
+                    if(board[i][j]){
+                        str = str + "Q" ;
+                    } else {
+                        str = str + ".";
+                    }
+                }
+                temp.add(str);
+            }
+            res.add(new ArrayList(temp));
+        }
+
+        for(int col = 0;col<n;col++){
+            if(Check(row, col, board)){
+                board[row][col] = true;
+                solve(row+1, n, board, res);
+                board[row][col] = false;
+            }
+        }
+    }
+
+    private boolean Check(int row, int col, boolean[][] board) {
+        for (int i = 0; i < board[0].length; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+        for (int c = col, r = row; c >= 0 && r >= 0; c--, r--) {
+            if (board[r][c]) {
+                return false;
+            }
+        }
+        for (int c = col, r = row; c < board.length && r >= 0; c++, r--) {
+            if (board[r][c]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
